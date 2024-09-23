bc18da19888ba95356811cdfd3945f2a0f1c7ec7
Trailblazer-dev <richvictor830@gmail.com>
2024-09-23 18:14:50 +0300

    building

--- src/com/Trailblazer/Algorithms/linkedList/Singlelinkedlist.java
+++ src/com/Trailblazer/Algorithms/linkedList/Singlelinkedlist.java
@@ -1,5 +1,7 @@
 package com.Trailblazer.Algorithms.linkedList;
 
+import java.util.List;
+
 /**
  * a linked list is a linear data structure where elements are stored in nodes that
  * are linked together using pointer
@@ -28,5 +30,50 @@ public class Singlelinkedlist {
             this.data=data;
             this.next=null;
         }
+    }
+
+    /**
+     * This method is used to display linked list
+      */
+    public void display(){
+
+        ListNode current =head;
+        while(current!=null){
+
+            System.out.print(current.data+" ===> ");
+            current = current.next;
+
+        }
+        System.out.println("null");
+    }
+
+    public int length(){
+        if(head==null){
+            return 0;
+        }
+        int count=0;
+        ListNode current =head;
+        while(current!= null){
+            count++;
+            current = current.next;
+
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Singlelinkedlist sll= new Singlelinkedlist();//instance of the class
+        sll.head= new ListNode(10);
+        ListNode second = new ListNode(1);
+        ListNode third = new ListNode(8);
+        ListNode four = new ListNode(11);
+//        now we will connect them together to form a chain
+        sll.head.next= second;
+        second.next=third;
+        third.next=four;
+
+        sll.display();
+        System.out.println("Length of the node list is "+sll.length());
+
     }
 }
